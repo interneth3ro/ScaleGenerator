2d43393ff8c51d56fa5616d0442ffb7a915b291a
James McConnell <JamesM@Magenic.net>
2016-07-05 17:00:17 -0500

    Moved scale generator component to home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,47 +1,38 @@
 import { Component } from '@angular/core';
-
-import { AppState } from '../app.service';
-import { Title } from './title';
-import { XLarge } from './x-large';
+import { NgForm } from '@angular/common';
+import { ScaleService } from './scale.service';
 
 @Component({
-  // The selector is what angular internally uses
-  // for `document.querySelectorAll(selector)` in our index.html
-  // where, in this case, selector is the string 'home'
-  selector: 'home',  // <home></home>
-  // We need to tell Angular's Dependency Injection which providers are in our app.
-  providers: [
-    Title
-  ],
-  // We need to tell Angular's compiler which directives are in our template.
-  // Doing so will allow Angular to attach our behavior to an element
-  directives: [
-    XLarge
-  ],
-  // We need to tell Angular's compiler which custom pipes are in our template.
-  pipes: [ ],
-  // Our list of styles in our component. We may add more to compose many styles together
+  selector: 'home',
+  providers: [ScaleService],
+  directives: [],
+  pipes: [],
   styleUrls: [ './home.style.css' ],
-  // Every Angular template is first compiled by the browser before Angular runs it's compiler
   templateUrl: './home.template.html'
 })
 export class Home {
-  // Set our default values
-  localState = { value: '' };
-  // TypeScript public modifiers
-  constructor(public appState: AppState, public title: Title) {
-
+  currentTuning: string = 'standard';
+  notes: string[];
+  modes: string[];
+  strings: IString[] = [];
+  model = {
+    tuning: 'standard',
+    note: 'E',
+    mode: 'Major'
+  };
+  
+  constructor(public scaleService: ScaleService) {
+    this.notes = this.scaleService.getChromaticForNote('E');
+    this.modes = this.scaleService.getModes();
+    this.strings = this.scaleService.getStrings(this.currentTuning);
   }
 
-  ngOnInit() {
-    console.log('hello `Home` component');
-    // this.title.getData().subscribe(data => this.data = data);
+  onSubmit(form) {
+    let scaleName: IScale = {
+      name: form.value.note,
+      mode: form.value.mode,
+      stringTuning: form.value.tuning
+    };
+    this.strings = this.scaleService.getScale(scaleName);
   }
-
-  submitState(value) {
-    console.log('submitState', value);
-    this.appState.set('value', value);
-    this.localState.value = '';
-  }
-
 }
